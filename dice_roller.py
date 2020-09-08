00ab58af6381e8b42eddd99d2b7825f4f0def1dd
SINGULARITY-101 <49837654+SINGULARITY-101@users.noreply.github.com>
2020-09-08 14:33:58 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,14 +1,15 @@
 import random 
-dice_rolls=2
+dice_rolls=int(input("How many times would you like to roll?"))
+dice_size=int(input("How many sides are there on the dice?"))
 dice_sum=0
 def main():
    for i in range(0,dice_rolls):
-      roll=random.randint(1,6)
+      roll=random.randint(1,dice_size)
       dice_sum=dice_sum+roll
       if roll==1:
          print(f"You rolled a {roll}! Critical fail")
          
-      elif roll==6:
+      elif roll==dice_size:
          print(f"You rolled  a {roll}! Critical Success")
          
       else:
